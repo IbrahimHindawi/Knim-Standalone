1af8972dc7232520de950e21308114c0e840a859
ibra <ihindawi@gmail.com>
2021-05-14 19:33:57 +0400

    hacking at kinc maths

--- Knim/kinc/math/matrix.nim
+++ Knim/kinc/math/matrix.nim
@@ -36,73 +36,120 @@ proc kinc_matrix4x4_multiply*(a: ptr kinc_matrix4x4_t; b: ptr kinc_matrix4x4_t):
 
 
 proc kinc_matrix4x4_identity*(): kinc_matrix4x4_t = 
-  var view: kinc_matrix4x4_t
-
-  kinc_matrix4x4_set(view.addr, 0, 0, 1)
-  kinc_matrix4x4_set(view.addr, 0, 1, 0)
-  kinc_matrix4x4_set(view.addr, 0, 2, 0)
-  kinc_matrix4x4_set(view.addr, 0, 3, 0)
+  kinc_matrix4x4_set(result.addr, 0, 0, 1)
+  kinc_matrix4x4_set(result.addr, 0, 1, 0)
+  kinc_matrix4x4_set(result.addr, 0, 2, 0)
+  kinc_matrix4x4_set(result.addr, 0, 3, 0)
   
-  kinc_matrix4x4_set(view.addr, 1, 0, 0)
-  kinc_matrix4x4_set(view.addr, 1, 1, 1)
-  kinc_matrix4x4_set(view.addr, 1, 2, 0)
-  kinc_matrix4x4_set(view.addr, 1, 3, 0)
+  kinc_matrix4x4_set(result.addr, 1, 0, 0)
+  kinc_matrix4x4_set(result.addr, 1, 1, 1)
+  kinc_matrix4x4_set(result.addr, 1, 2, 0)
+  kinc_matrix4x4_set(result.addr, 1, 3, 0)
 
-  kinc_matrix4x4_set(view.addr, 2, 0, 0)
-  kinc_matrix4x4_set(view.addr, 2, 1, 0)
-  kinc_matrix4x4_set(view.addr, 2, 2, 1)
-  kinc_matrix4x4_set(view.addr, 2, 3, 0)
+  kinc_matrix4x4_set(result.addr, 2, 0, 0)
+  kinc_matrix4x4_set(result.addr, 2, 1, 0)
+  kinc_matrix4x4_set(result.addr, 2, 2, 1)
+  kinc_matrix4x4_set(result.addr, 2, 3, 0)
 
-  kinc_matrix4x4_set(view.addr, 3, 0, 0)
-  kinc_matrix4x4_set(view.addr, 3, 1, 0)
-  kinc_matrix4x4_set(view.addr, 3, 2, 0)
-  kinc_matrix4x4_set(view.addr, 3, 3, 1)
+  kinc_matrix4x4_set(result.addr, 3, 0, 0)
+  kinc_matrix4x4_set(result.addr, 3, 1, 0)
+  kinc_matrix4x4_set(result.addr, 3, 2, 0)
+  kinc_matrix4x4_set(result.addr, 3, 3, 1)
+
+  
 
 
 proc kinc_matrix4x4_perspective*( fov: float, aspect: float,
                                   near: float, far: float): kinc_matrix4x4_t =
   var 
-    m: kinc_matrix4x4_t
     uh: float = kinc_cot(fov/2.0)
     uw: float = uh / aspect
-  kinc_matrix4x4_set(m.addr, 0, 0, uw)
-  kinc_matrix4x4_set(m.addr, 1, 1, uh)
-  kinc_matrix4x4_set(m.addr, 2, 2, ((far + near) / (far - near)))
-  kinc_matrix4x4_set(m.addr, 3, 2, 1)
-  return m
+  result = kinc_matrix4x4_identity()
+  kinc_matrix4x4_set(result.addr, 0, 0, uw)
+  kinc_matrix4x4_set(result.addr, 1, 1, uh)
+  kinc_matrix4x4_set(result.addr, 2, 2, (far + near) / (far - near) )
+  kinc_matrix4x4_set(result.addr, 2, 3, -((2 * far * near) / (far - near)))
+  kinc_matrix4x4_set(result.addr, 3, 2, 1)
 
-proc kinc_matrix4x4_look_at*( eye: kinc_vector3_t, at: kinc_vector3_t, up: kinc_vector3_t): kinc_matrix4x4_t =
+# proc kinc_matrix4x4_look_at*( eye,center,up: kinc_vector3_t): kinc_matrix4x4_t =
+#   var
+#     zaxis = kinc_vector3_normalize(kinc_vector3_subtract(center, eye))
+#     xaxis = kinc_vector3_normalize(kinc_vector3_cross(up, zaxis))
+#     yaxis = kinc_vector3_cross(zaxis, xaxis)
+
+#   result = kinc_matrix4x4_identity()
+#   kinc_matrix4x4_set(result.addr, 0, 0, xaxis.x)
+#   kinc_matrix4x4_set(result.addr, 0, 1, xaxis.y)
+#   kinc_matrix4x4_set(result.addr, 0, 2, xaxis.z)
+#   kinc_matrix4x4_set(result.addr, 0, 3, kinc_vector3_dot(xaxis, eye) * -1)
   
-  var zaxis: kinc_vector3_t = kinc_vector3_t_subtract(at, eye)
-  zaxis = kinc_vector3_t_normalize(zaxis)
+#   kinc_matrix4x4_set(result.addr, 1, 0, yaxis.x)
+#   kinc_matrix4x4_set(result.addr, 1, 1, yaxis.y)
+#   kinc_matrix4x4_set(result.addr, 1, 2, yaxis.z)
+#   kinc_matrix4x4_set(result.addr, 1, 3, kinc_vector3_dot(yaxis, eye) * -1)
 
-  var xaxis: kinc_vector3_t = kinc_vector3_t_cross(up, zaxis)
-  xaxis = kinc_vector3_t_normalize(xaxis)
+#   kinc_matrix4x4_set(result.addr, 2, 0, zaxis.x)
+#   kinc_matrix4x4_set(result.addr, 2, 1, zaxis.y)
+#   kinc_matrix4x4_set(result.addr, 2, 2, zaxis.z)
+#   kinc_matrix4x4_set(result.addr, 2, 3, kinc_vector3_dot(zaxis, eye) * -1)
 
-  var yaxis: kinc_vector3_t = kinc_vector3_t_cross(zaxis, xaxis)
-  var view: kinc_matrix4x4_t
+#   kinc_matrix4x4_set(result.addr, 3, 0, 0)
+#   kinc_matrix4x4_set(result.addr, 3, 1, 0)
+#   kinc_matrix4x4_set(result.addr, 3, 2, 0)
+#   kinc_matrix4x4_set(result.addr, 3, 3, 1)
 
-  kinc_matrix4x4_set(view.addr, 0, 0, xaxis.x)
-  kinc_matrix4x4_set(view.addr, 0, 1, xaxis.y)
-  kinc_matrix4x4_set(view.addr, 0, 2, xaxis.z)
-  kinc_matrix4x4_set(view.addr, 0, 3, kinc_vector3_t_dot(xaxis, eye) * -1)
+# LH
+  # proc kinc_matrix4x4_look_at*( eye,center,up: kinc_vector3_t): kinc_matrix4x4_t =
+  # var
+  #   f = kinc_vector3_normalize(kinc_vector3_subtract(center, eye))
+  #   s = kinc_vector3_normalize(kinc_vector3_cross(up, f))
+  #   u = kinc_vector3_cross(f, s)
+
+  # result = kinc_matrix4x4_identity()
+  # kinc_matrix4x4_set(result.addr, 0, 0, s.x)
+  # kinc_matrix4x4_set(result.addr, 0, 1, u.x)
+  # kinc_matrix4x4_set(result.addr, 0, 2, f.x)
+  # #kinc_matrix4x4_set(result.addr, 0, 3, kinc_vector3_dot(s, eye) * -1)
   
-  kinc_matrix4x4_set(view.addr, 1, 0, yaxis.x)
-  kinc_matrix4x4_set(view.addr, 1, 1, yaxis.y)
-  kinc_matrix4x4_set(view.addr, 1, 2, yaxis.z)
-  kinc_matrix4x4_set(view.addr, 1, 3, kinc_vector3_t_dot(yaxis, eye) * -1)
+  # kinc_matrix4x4_set(result.addr, 1, 0, s.y)
+  # kinc_matrix4x4_set(result.addr, 1, 1, u.y)
+  # kinc_matrix4x4_set(result.addr, 1, 2, f.y)
+  # #kinc_matrix4x4_set(result.addr, 1, 3, kinc_vector3_dot(u, eye) * -1)
 
-  kinc_matrix4x4_set(view.addr, 2, 0, zaxis.x)
-  kinc_matrix4x4_set(view.addr, 2, 1, zaxis.y)
-  kinc_matrix4x4_set(view.addr, 2, 2, zaxis.z)
-  kinc_matrix4x4_set(view.addr, 2, 3, kinc_vector3_t_dot(zaxis, eye) * -1)
+  # kinc_matrix4x4_set(result.addr, 2, 0, s.z)
+  # kinc_matrix4x4_set(result.addr, 2, 1, u.z)
+  # kinc_matrix4x4_set(result.addr, 2, 2, f.z)
+  # #kinc_matrix4x4_set(result.addr, 2, 3, kinc_vector3_dot(f, eye) * -1)
 
-  kinc_matrix4x4_set(view.addr, 3, 0, 0)
-  kinc_matrix4x4_set(view.addr, 3, 1, 0)
-  kinc_matrix4x4_set(view.addr, 3, 2, 0)
-  kinc_matrix4x4_set(view.addr, 3, 3, 1)
+  # kinc_matrix4x4_set(result.addr, 3, 0, kinc_vector3_dot(s, eye) * -1)
+  # kinc_matrix4x4_set(result.addr, 3, 1, kinc_vector3_dot(u, eye) * -1)
+  # kinc_matrix4x4_set(result.addr, 3, 2, kinc_vector3_dot(f, eye) * -1)
+  # kinc_matrix4x4_set(result.addr, 3, 3, 1)
 
-  view
+# RH
+proc kinc_matrix4x4_look_at*( eye,center,up: kinc_vector3_t): kinc_matrix4x4_t =
+  var
+    f = kinc_vector3_normalize(kinc_vector3_subtract(center, eye))
+    s = kinc_vector3_normalize(kinc_vector3_cross(f, up))
+    u = kinc_vector3_cross(s, f)
+
+  result = kinc_matrix4x4_identity()
+  kinc_matrix4x4_set(result.addr, 0, 0, s.x)
+  kinc_matrix4x4_set(result.addr, 0, 1, u.x)
+  kinc_matrix4x4_set(result.addr, 0, 2, -f.x)
+  #kinc_matrix4x4_set(result.addr, 0, 3, kinc_vector3_dot(s, eye) * -1)
   
+  kinc_matrix4x4_set(result.addr, 1, 0, s.y)
+  kinc_matrix4x4_set(result.addr, 1, 1, u.y)
+  kinc_matrix4x4_set(result.addr, 1, 2, -f.y)
+  #kinc_matrix4x4_set(result.addr, 1, 3, kinc_vector3_dot(u, eye) * -1)
 
-  
+  kinc_matrix4x4_set(result.addr, 2, 0, s.z)
+  kinc_matrix4x4_set(result.addr, 2, 1, u.z)
+  kinc_matrix4x4_set(result.addr, 2, 2, -f.z)
+  #kinc_matrix4x4_set(result.addr, 2, 3, kinc_vector3_dot(f, eye) * -1)
+
+  kinc_matrix4x4_set(result.addr, 3, 0, kinc_vector3_dot(s, eye) * -1)
+  kinc_matrix4x4_set(result.addr, 3, 1, kinc_vector3_dot(u, eye) * -1)
+  kinc_matrix4x4_set(result.addr, 3, 2, kinc_vector3_dot(f, eye) * -1)
+  #kinc_matrix4x4_set(result.addr, 3, 3, 1)
--- Knim/kinc/math/vector.nim
+++ Knim/kinc/math/vector.nim
@@ -19,28 +19,31 @@ type
     z*: cfloat
     w*: cfloat
 
-proc kinc_vector3_t_new*(x, y, z: cfloat): kinc_vector3_t =
+proc kinc_vector3_new*(x, y, z: cfloat): kinc_vector3_t =
   var vec: kinc_vector3_t
   vec.x = x
   vec.y = y
   vec.z = z
   vec
 
-proc kinc_vector3_t_subtract*(a, b: kinc_vector3_t): kinc_vector3_t =
-  result = kinc_vector3_t_new(a.x - b.x, a.y - b.y, a.z - b.z)
+proc kinc_vector3_subtract*(a, b: kinc_vector3_t): kinc_vector3_t =
+  result = kinc_vector3_new(a.x - b.x, a.y - b.y, a.z - b.z)
 
-proc kinc_vector3_t_cross*(a, b: kinc_vector3_t): kinc_vector3_t = 
-  result = kinc_vector3_t_new(
+proc kinc_vector3_cross*(a, b: kinc_vector3_t): kinc_vector3_t = 
+  result = kinc_vector3_new(
     a.y * b.z - a.z * b.y,
     a.z * b.x - a.x * b.z,
     a.x * b.y - a.y * b.x)
 
-proc kinc_vector3_t_dot*(a, b: kinc_vector3_t): cfloat =
+proc kinc_vector3_mult_scalar*(a:kinc_vector3_t, b: cfloat): kinc_vector3_t =
+  result = kinc_vector3_new(a.x * b, a.y / b, a.z / b)
+
+proc kinc_vector3_dot*(a, b: kinc_vector3_t): cfloat =
   result = a.x * b.x + a.y * b.y + a.z * b.z
 
-proc kinc_vector3_t_len*(v: kinc_vector3_t): float32 =
+proc kinc_vector3_len*(v: kinc_vector3_t): cfloat =
   result = sqrt(v.x * v.x + v.y * v.y + v.z * v.z)
 
-proc kinc_vector3_t_normalize*(v: kinc_vector3_t): kinc_vector3_t =
-  let len = v.kinc_vector3_t_len
-  result = kinc_vector3_t_new(v.x / len, v.y / len, v.z / len)
+proc kinc_vector3_normalize*(v: kinc_vector3_t): kinc_vector3_t =
+  let len = v.kinc_vector3_len
+  result = kinc_vector3_new(v.x / len, v.y / len, v.z / len)
